8f6b45c2b52f7a90bafeb5b5c8db552bc6441b49
Jeremy Burton <jermbo002@gmail.com>
2012-07-18 11:35:14 -0500

    First upload

---
+++ slide_config.js
@@ -0,0 +1,30 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'SEO Optimization',
+    subtitle: 'With Google Analytics &amp; Webmaster Tools',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/si-logo-tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Inconsolata'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Jeremy Burton',
+    company: 'Owner &amp; Lead Developer, Select Interactive',
+    email: 'jeremy@select-interactive.com',
+    gplus: 'https://plus.google.com/u/0/106812648379440327225/',
+    twitter: '@Sel_Interactive',
+    www: 'http://www.select-interactive.com'
+    //github: 'https://github.com/jermbo002'
+  }]
+};
+
